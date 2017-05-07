f809d366bf7d7ef65600deb7efecacdc06cd42c7
Nathan Olberding <Nathan Olberding>
2017-05-06 17:39:41 -0700

    Adding Windows directory.

---
+++ Windows/Profile.ps1
@@ -0,0 +1,8 @@
+﻿#Profile.ps1
+#
+# This script is meant to run whenever a new interactive Powershell sessions
+# is started.
+
+# Create transcript logs.
+if (!(ls $env:USERPROFILE\logs -EA SilentlyContinue)) {New-Item -Name logs -Path $env:USERPROFILE -ItemType directory}
+Start-Transcript -Path "$env:USERPROFILE\logs\$(get-date -UFormat %Y-%m-%d).log" -Append -NoClobber
